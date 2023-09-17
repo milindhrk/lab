0c781d58c033118c6ba4aee33c68594a8ee4f9cf
milindhrk <86625575+milindhrk@users.noreply.github.com>
2023-09-16 20:00:51 -0400

    Add files via upload

---
+++ src/model/Product.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Milindh R Kashyap
+ */
+public class Product 
+{
+    private String name;
+    private String description;
+    private String availNum;
+    private String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAvailNum() {
+        return availNum;
+    }
+
+    public void setAvailNum(String availNum) {
+        this.availNum = availNum;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    
+    
+}
